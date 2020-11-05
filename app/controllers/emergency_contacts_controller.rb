a3da8fc571e9c0aa5ce6b74b5718d6ac7eebbce3
bnddiep <bach.nhu.diep@gmail.com>
2020-11-04 16:12:14 -0800

    Adds the controller methods for personal info and emergency contact. Created mockPersonalInfo data and added to state object in App.js

--- app/controllers/emergency_contacts_controller.rb
+++ app/controllers/emergency_contacts_controller.rb
@@ -1,2 +1,39 @@
 class EmergencyContactsController < ApplicationController
+    def index
+        @contacts = EmergencyContact.all
+        render json: contacts
+    end
+
+    def create
+        @contact = EmergencyContact.create(emergency_contact_params)
+        if contact.valid?
+            render json: contact
+        else
+            render json: contact.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @contact = current_user.emergency_contacts.find(params[:id])
+        @contact.update(emergency_contact_params)
+        if contact.valid?
+            render json: contact
+        else
+            render json: contact.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @contact = current_user.emergency_contacts.find(params[:id])
+        if contact.destroy
+            render json: contact
+        else
+            render json: contact.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def emergency_contact_params
+        params.require(:emergency_contact).permit(:full_name, :relationship, :phone_number)
+    end
 end
--- app/controllers/personal_infos_controller.rb
+++ app/controllers/personal_infos_controller.rb
@@ -1,2 +1,39 @@
 class PersonalInfosController < ApplicationController
+    def index
+        @infos = PersonalInfo.all
+        render json: infos
+    end
+
+    def create
+        @info = PersonalInfo.create(personal_info_params)
+        if info.valid?
+            render json: info
+        else
+            render json: info.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @info = current_user.personal_infos.find(params[:id])
+        @info.update(personal_info_params)
+        if info.valid?
+            render json: info
+        else
+            render json: info.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @info = current_user.personal_infos.find(params[:id])
+        if info.destroy
+            render json: info
+        else
+            render json: info.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def personal_info_params
+        params.require(:personal_info).permit(:full_name, :gender, :phone_number, :allergy, :medical_history, :treatment_refusal, :pregnancy_status, :smoker, :alcohol, :recreational_drug, :menstruation, :code_status, :medical_condition, :diagnosis_date, :medication, :dosage)
+    end
 end
