5c3cc15c03f963386ef08006562dd3aededd7bde
bnddiep <bach.nhu.diep@gmail.com>
2020-11-12 23:43:48 -0800

    added illustration that may intrigue users to user app

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import lifeband from '../assets/lifeband-home.png'
 
 
 class Home extends Component {
@@ -8,7 +8,8 @@ class Home extends Component {
       <>
         <h3>
           This is the Home.
-      </h3>
+        </h3>
+        <img src={lifeband} alt="hiker and foodie illustration" />
       </>
     )
   }
