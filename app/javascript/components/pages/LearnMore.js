e1fd1d4ed429cc1ae22a9432a278de58c9bdf04e
DiemBTran <diembtran@gmail.com>
2020-11-17 13:02:25 -0800

    Edits Learn More

--- app/javascript/components/pages/LearnMore.js
+++ app/javascript/components/pages/LearnMore.js
@@ -7,25 +7,25 @@ class LearnMore extends Component {
     return (
       <>
         <h3>What is LifeBand?</h3>
-        <h4>The Problem</h4>
+        <h5>The Problem</h5>
         <p>
-        Any individual who has their ailments knows their own condition better than anyone else. However, because of this, many feel they no longer can trust leaving the area in which they reside in for fear of needing medical attention from professionals who do not know them or their needs…
+          Any individual who is afflicted with medical ailments and allergies knows their own condition better than anyone else. Oftentimes, when a person is brought incapcitated or nonverbal to the ER, their loved ones are not able to provide medical professionals the vital information necessary to give the individual the best possible care. Because of this, many people live in fear, choosing to not pursue risky activities or even eat at new restaurants in case the need for medical attention arises with professionals who do not know them or their needs.
         </p>
+        <h5>The Solution: <span>LifeBand!</span></h5>
         <p>
-          The Solution?
+          LifeBand is a simple but life changing tool that we have developed to help give each individual the opportunity to take their life back from their ailments. We here at Life Science Rejects understand that every person is unique in their medical needs and that they must be communicated, no matter what! When you create an account with LifeBand, you are simply filling out a form giving all important information that YOU as the patient would like to be known. A QR code is then generated for you to share with your loved ones that redirects to your profile. (As such, since you are making this information available to anybody who can access your profile’s QR code and to our domain, you are releasing whatever personal information you submit under the Health Insurance Portability and Accountability Act [HIPPA]) You can edit any of your medical information and emergency contacts at any time, too, so you can always control what information you want to make available.
         </p>
-        <span>LifeBand!</span>
-        <h4>What is LifeBand?</h4>
+        <h5>List of Emergency Contacts</h5>
         <p>
-        LifeBand is a simple, but life changing tool that we have developed to help give each individual the opportunity to take their life back from their ailments. We, here, at LifeScience Rejects understand that every person is unique in their medical needs, and it must be communicated, no matter what! When you create an account with LifeBand, you are simply filling out a form giving all important information that YOU as the patient would like to be known.
+          Let’s face it, we ALL need to be prepared for any emergency. Having a list of Emergency Contacts is one of the best ways we can be prepared for any scenario, especially for scenarios where we need others to speak for us! As such, we would all do well by having loved ones who know us personally at the ready in case any emergency occurs, and LifeBand allows you to have that list all in one place.
         </p>
-        <h4>List of Emergency Contacts:</h4>
+        <h5>List of Current Daily Medications</h5>
         <p>
-        Let’s face it; we ALL need to be prepared for any emergency. Having a list of Emergency Contacts is one of the best ways we can be prepared for any scenario; especially for scenarios where we need others to speak for us! In this case, we would do well in having loved ones at the ready who know us personally, and LifeBand allows you to have that list all in one place.
+          Every visit to the ER requires the medical staff to know exactly what medications you are currently taking every single day. Why not keep a list of this as well? LifeBand provides a section for you to fill out any of your daily medications that you would like to make public so that medical professionals can see any potential drug interactions that may arise in your care.
         </p>
-        <h4>List of Current Daily Medications:</h4>
+        <h5>Not sure if you want to sign up?</h5>
         <p>
-        Every visit to the ER requires the medical staff to know exactly what medications you are currently taking every single day. Why not keep a list of this as well?
+          View our demo page to see what your new public profile could look like! When you make your account, the QR code to your <span>public profile</span> can be found on your <span>private profile</span>. We urge you to take care of yourself and be an advocate for your health, so let’s <span>band up on LifeBand!</span>
         </p>
       </>
     )
